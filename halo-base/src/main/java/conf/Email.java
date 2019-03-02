950035def9b3338e8fd3cde1d3482609fe561227
MelloYuki <352983198@qq.com>
2019-03-02 23:08:58 +0800

    :ok_hand:

---
+++ halo-base/src/main/java/conf/Email.java
@@ -0,0 +1,54 @@
+package conf;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author MelloChan
+ * @date 2018/5/31
+ */
+@Component
+public class Email {
+
+    @Value("${halo.smtp}")
+    private String smtp;
+
+    @Value("${halo.email}")
+    private String email;
+
+    @Value("${halo.password}")
+    private String password;
+
+    public String getSmtp() {
+        return smtp;
+    }
+
+    public void setSmtp(String smtp) {
+        this.smtp = smtp;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Email{" +
+                "smtp='" + smtp + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ halo-base/src/main/java/dto/OrderDetailDTO.java
@@ -0,0 +1,65 @@
+package dto;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author MelloChan
+ * @date 2018/6/10
+ */
+public class OrderDetailDTO implements Serializable{
+    private static final long serialVersionUID = 673372102167297677L;
+
+    @NotBlank
+    private String id;
+    private ReceiverInfoDTO receiver;
+    private List<OrderProductDTO>products;
+    @Min(1)
+    @Max(3)
+    private Short payType;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public ReceiverInfoDTO getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(ReceiverInfoDTO receiver) {
+        this.receiver = receiver;
+    }
+
+    public List<OrderProductDTO> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<OrderProductDTO> products) {
+        this.products = products;
+    }
+
+    public Short getPayType() {
+        return payType;
+    }
+
+    public void setPayType(Short payType) {
+        this.payType = payType;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDetailDTO{" +
+                "id=" + id +
+                ", receiver=" + receiver +
+                ", products=" + products +
+                ", payType=" + payType +
+                '}';
+    }
+}

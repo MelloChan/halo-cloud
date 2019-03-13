09f8662258232253820b57656db5597f7be3b32d
MelloYuki <352983198@qq.com>
2019-03-13 21:51:16 +0800

    :fire:

---
+++ halo-forum-server/src/main/java/com/halo/cloud/forum/server/dao/NotifyDao.java
@@ -0,0 +1,13 @@
+package com.halo.cloud.forum.server.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Author: mellochan[陈文铭]
+ * @Date: 2019/3/13 21:48
+ * @Version 1.0
+ */
+@Repository
+public interface NotifyDao {
+
+}
